8332eed63c032fe6edff96bd301b355d1c3f31bc
Diogo Sequeira <diogo.sequeira@ctw.bmwgroup.com>
2022-10-02 23:06:44 +0100

    add remaining endpoints

--- lib/repository/endpoints.dart
+++ lib/repository/endpoints.dart
@@ -4,10 +4,18 @@ import 'dart:io';
 @immutable
 class Endpoints {
   /// infura.io Ethereum Kovan DEV EP
-  static String infuraUrl() => 'https://kovan.infura.io/v3/701c0bf7067e4979865d0122e2672075';
+  static String infuraApiUrl() => 'https://kovan.infura.io/v3/701c0bf7067e4979865d0122e2672075';
 
   /// infura's WebSocket url
   static String infuraWsUrl() => 'wss://kovan.infura.io/ws/v3/701c0bf7067e4979865d0122e2672075';
+
+  /// Remote Procedure Call [RPC Server] from Ganache
+  static String ganacheApiUrl() =>
+      Platform.isAndroid ? 'http://10.0.2.2:7545' : 'http://127.0.0.1:7545';
+
+  /// WebSocket Url
+  static String ganacheWsUrl() =>
+      Platform.isAndroid ? 'http://10.0.2.2:7545' : 'ws://127.0.0.1:7545/';
 
   /// Never use private keys in production, use walletconnect + metamask instead
   static String mockPrivateGanacheKey() =>
