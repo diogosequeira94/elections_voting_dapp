530eaed116d3ec89edb85b872de1555c8726a46b
Diogo Sequeira <diogo.sequeira@ctw.bmwgroup.com>
2022-10-02 23:31:16 +0100

    refactor code on web3api

--- lib/repository/election_web3_api_client.dart
+++ lib/repository/election_web3_api_client.dart
@@ -8,7 +8,7 @@ import 'package:web_socket_channel/io.dart';
 
 class ElectionWeb3Client {
   late Web3Client ethWeb3client;
- // late ElectionDeployedContract _electionDeployedContract;
+  late DeployedContract deployedContract;
   late ContractAbi _contractAbiCode;
   late EthereumAddress _contractAddress;
   late EthPrivateKey _credentials;
@@ -16,41 +16,40 @@ class ElectionWeb3Client {
   static const electionContractPath = 'assets/contracts/election.json';
 
   EthPrivateKey get getCredentials => _credentials;
- // ElectionDeployedContract get getNotesDeployedContract => _electionDeployedContract;
 
   Future<void> init() async {
     ethWeb3client = Web3Client(
       Endpoints.ganacheApiUrl(),
       Client(),
       socketConnector: () {
-        return IOWebSocketChannel.connect(Endpoints.ganacheWsUrl()).cast<String>();
+        return IOWebSocketChannel.connect(Endpoints.ganacheWsUrl())
+            .cast<String>();
       },
     );
-   // _electionDeployedContract = ElectionDeployedContract();
-    await _getABI();
-    await _getAddress(_contractAbiCode);
-    await _getCredentials();
-   // await _electionDeployedContract.initContract(_contractAbiCode, _contractAddress);
+    _contractAbiCode = await _getABI();
+    _contractAddress = await _getAddress(_contractAbiCode);
+    _credentials = await _getCredentials();
+    deployedContract = await loadContract();
   }
 
-  Future<void> _getABI() async {
+  Future<ContractAbi> _getABI() async {
     final abiFile = await rootBundle.loadString(electionContractPath);
     final jsonABI = jsonDecode(abiFile);
-    _contractAbiCode =
-        ContractAbi.fromJson(jsonEncode(jsonABI['abi']), 'Election');
+    return ContractAbi.fromJson(jsonEncode(jsonABI['abi']), 'Election');
   }
 
-  Future<void> _getAddress(ContractAbi contractAbi) async {
+  Future<EthereumAddress> _getAddress(ContractAbi contractAbi) async {
     final abiFile = await rootBundle.loadString(electionContractPath);
     final jsonABI = jsonDecode(abiFile);
-    _contractAddress =
-        EthereumAddress.fromHex(jsonABI["networks"]["5777"]["address"]);
+    return EthereumAddress.fromHex(jsonABI["networks"]["5777"]["address"]);
   }
 
-  Future<void> _getCredentials() async {
-    _credentials = EthPrivateKey.fromHex(Endpoints.ownerPrivateKey());
-    // /// Test logs
-    // EtherAmount balance = await web3client.getBalance(_credentials.address);
-    // print(balance.getValueInUnit(EtherUnit.ether));
+  Future<EthPrivateKey> _getCredentials() async {
+    return EthPrivateKey.fromHex(Endpoints.ownerPrivateKey());
+  }
+
+  Future<DeployedContract> loadContract() async {
+    final contract = DeployedContract(_contractAbiCode, _contractAddress);
+    return contract;
   }
 }
