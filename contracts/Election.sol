013429d3346ca100f25c5c43e4bae19a085ec671
Diogo Sequeira <diogo.sequeira@ctw.bmwgroup.com>
2022-10-02 19:30:10 +0100

    updated election contract

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -22,8 +22,40 @@ contract Election {
     Candidate[] public candidates;
     uint public totalVotes;
 
-
-    function startElection(){
-
+    /// Owner is who triggered the election
+    function startElection(string memory _electionName) public {
+        owner = msg.sender;
+        electionName = _electionName;
     }
+
+    modifier ownerOnly() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    /// Only owner can addCandidates
+    function addCandidate(string memory _candidateName) ownerOnly public {
+        candidates.push(Candidate(_candidateName, 0));
+    }
+
+    function getCandidatesNumber() public view returns(uint){
+        return candidates.length;
+    }
+
+    function authorizeVoter(address _voterAddress) ownerOnly public {
+        voters[_voterAddress].isAuthorised = true;
+    }
+
+    function vote(uint candidateIndex) public {
+        /// Needs to be authorized to vote
+        require(voters[msg.sender].isAuthorised);
+        /// Check if voter has not voted yet
+        require(!voters[msg.sender].hasVoted);
+        voters[msg.sender].candidateChosen = candidateIndex;
+        voters[msg.sender].hasVoted = true;
+
+        /// Increase candidate vote count by 1
+        candidates[candidateIndex].votesNumber++;
+    }
+
 }
