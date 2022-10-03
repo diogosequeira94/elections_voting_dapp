912e6bf52ffd81965722452c5507e088ba7cfaa6
Diogo Sequeira <diogosequeira@hotmail.fr>
2022-10-03 12:15:36 +0100

    add elections

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -5,15 +6,53 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    TextEditingController controller = TextEditingController();
     return Scaffold(
       appBar: AppBar(
         title: const Text('Elections Dapp Demo'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          const Text('Hello!'),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/governance-image.png',
+                width: 200,
+                height: 200,
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 32.0),
+                child: Text(
+                  'Welcome to the elections!',
+                  style: TextStyle(fontSize: 24.0),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              TextField(
+                controller: controller,
+                decoration: const InputDecoration(
+                    filled: true, hintText: 'Enter election name'),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top:24.0),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 45,
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      if (controller.text.isNotEmpty) {
+                        print('Election Started!');
+                      }
+                    },
+                    child: const Text('Start Election'),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
