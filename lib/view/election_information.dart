c02d17cb117aefe23ad8c5cdc1e87bf77a0802b4
Diogo Sequeira <diogosequeira@hotmail.fr>
2022-10-04 00:07:30 +0100

    add election row

--- lib/view/election_information.dart
+++ lib/view/election_information.dart
@@ -1,16 +1,61 @@
 import 'package:flutter/material.dart';
 
-class ElectionInformation extends StatelessWidget {
+class ElectionInformation extends StatefulWidget {
   final String electionName;
-  const ElectionInformation({Key? key, required this.electionName}) : super(key: key);
+  const ElectionInformation({Key? key, required this.electionName})
+      : super(key: key);
 
+  @override
+  State<ElectionInformation> createState() => _ElectionInformationState();
+}
+
+class _ElectionInformationState extends State<ElectionInformation> {
+  late TextEditingController addCandidateController;
+
+  @override
+  void initState() {
+    addCandidateController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    addCandidateController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(electionName),
+        title: Text(widget.electionName),
       ),
-      body: Container(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 25.0),
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                key: const Key('electionInfo_candidateVotes'),
+                children: [
+                  Column(
+                    children: const [
+                      Text('Total Candidates'),
+                      Text('0'),
+                    ],
+                  ),
+                  Column(
+                    children: const [
+                      Text('Total Votes'),
+                      Text('0'),
+                    ],
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -28,7 +28,8 @@ class HomePage extends StatelessWidget {
             Navigator.push(
               context,
               MaterialPageRoute(
-                builder: (context) => ElectionInformation(electionName: state.electionName),
+                builder: (context) =>
+                    ElectionInformation(electionName: state.electionName),
               ),
             );
             // Navigate to election information page
@@ -56,7 +57,8 @@ class HomePage extends StatelessWidget {
                   ),
                   TextField(
                     controller: controller,
-                    decoration: const InputDecoration(filled: true, hintText: 'Enter election name'),
+                    decoration: const InputDecoration(
+                        filled: true, hintText: 'Enter election name'),
                   ),
                   Padding(
                     padding: const EdgeInsets.only(top: 24.0),
@@ -67,12 +69,14 @@ class HomePage extends StatelessWidget {
                         onPressed: () async {
                           if (controller.text.isNotEmpty) {
                             context.read<ElectionBloc>().add(
-                                  StartElectionPressed(electionName: controller.text),
+                                  StartElectionPressed(
+                                      electionName: controller.text),
                                 );
                           }
                         },
                         child: state is ElectionStartInProgress
-                            ? const CircularProgressIndicator()
+                            ? const CircularProgressIndicator(
+                                color: Colors.white)
                             : const Text('Start Election'),
                       ),
                     ),
